d04dc8da4cde5ae550ee3361afede7bf13249f8d
florent.descroix <florent.descroix@162.38.111.43>
2015-03-03 13:02:03 +0100

    Connection BD opérationnelle !!!

--- src/Persistance/DatabaseUtils.java
+++ src/Persistance/DatabaseUtils.java
@@ -2,38 +2,66 @@ package Persistance;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.sun.corba.se.impl.util.Version;
 
 /**
  */
 public abstract class DatabaseUtils {
 
-	private static Connection conn;
+	private static Connection conn = null;
+	private static Statement st = null;
+	private static ResultSet rs = null;
 	
-	private final static String server = "jdbc:postgresql://sql.free.fr:5432/mcse.zenlounge";
-	private final static String login = "mcse.zenlounge";
+	private final static String server = "jdbc:postgresql://mcse.heliohost.org:5432/mcse_zenlounge";
+	private final static String login = "mcse_zen";
 	private final static String password = ProtectedConfigFile.getPassword(1);
 	
 	/**
 	 * This is the database connection
 	 */
 	public static void connexion() {
-		
+       
 		try {
-			Class.forName("org.postgresql.Driver");
-			System.out.println("Driver O.K.");
+			//Class.forName("org.postgresql.Driver");
+			//System.out.println("Driver O.K.");
 
 			conn = DriverManager.getConnection(server, login, password);
-			System.out.println("Connexion effective !");
+			st = conn.createStatement();
+	        rs = st.executeQuery("SELECT * FROM information_schema.tables;");
 
-		} catch (Exception e) {
-			e.printStackTrace();
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+		} catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(Version.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
 		}
 	}
 	
 	/**
  */
 	public static void deconnexion() {
-		// your code here
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (st != null) {
+                st.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(Version.class.getName());
+            lgr.log(Level.WARNING, ex.getMessage(), ex);
+        }
 	}
 
 	/**
