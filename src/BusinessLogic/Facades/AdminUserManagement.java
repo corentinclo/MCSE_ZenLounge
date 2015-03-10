39d7bf1b438e033be28abef614a102b58b41a06e
Corentin <corentincloarec@hotmail.fr>
2015-03-10 19:07:09 +0100

    Nouvelles classes et méthodes pour ce usecase

---
+++ src/BusinessLogic/Facades/AdminUserManagement.java
@@ -0,0 +1,60 @@
+
+package BusinessLogic.Facades;
+
+/**
+ * 
+ * 
+ * @poseidon-object-id [I1dbea9a9m14bff6c00aam25af]
+ */
+public class AdminUserManagement {
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [I1dbea9a9m14bff6c00aam25c8]
+ * @param firstName 
+ * @param lastName 
+ * @param tel 
+ * @param email 
+ */
+    public void updateUser(String firstName, String lastName, int tel, String email) {        
+        // your code here
+    } 
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [I1dbea9a9m14bff6c00aam26e6]
+ * @param firstName 
+ * @param lastName 
+ * @param id 
+ */
+    public void searchUser(String firstName, String lastName, String id) {        
+        // your code here
+    } 
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [I1dbea9a9m14bff6c00aam2784]
+ * @param user 
+ */
+    public void deleteUser(User user) {        
+        // your code here
+    } 
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [I1dbea9a9m14bff6c00aam27c4]
+ * @param firstName 
+ * @param lastName 
+ * @param email 
+ * @param phone 
+ * @param id 
+ * @param password 
+ */
+    public void createUser(String firstName, String lastName, String email, int phone, String id, String password) {        
+        // your code here
+    } 
+ }
---
+++ src/Views/Admin_User_Management/PopupUpdateUser.java
@@ -0,0 +1,10 @@
+
+package Views.Admin_User_Management;
+
+/**
+ * 
+ * 
+ * @poseidon-object-id [I1dbea9a9m14bff6c00aam2a1f]
+ */
+public class PopupUpdateUser implements Views.AbstractView {
+ }
